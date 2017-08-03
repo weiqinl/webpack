7dff0970a83eac29c3e2a276e915d39e83e40af5
刘伟勤 <wbliuwq@yonyou.com>
2017-08-03 17:33:15 +0800

    加载css样式

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import _ from 'lodash';
+import './style.css';
 
 function component() {
 	var element = document.createElement('div');
 
-	// Lodash, 现在通过script imported
+	// Lodash 是由当前 script 脚本 import 导入进来的
 	element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+	// 给元素添加 hello 样式类
+	element.classList.add('hello');
 
 	return element;
 }
