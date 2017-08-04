1e46718b7b2bbd19886c6b06ea1775e8b2898134
刘伟勤 <wbliuwq@yonyou.com>
2017-08-04 13:40:21 +0800

    管理资源--加载CSS/图片/字体/数据/资源

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import _ from 'lodash';
 import './style.css';
+import Icon from './logo.png';
+import Data from './data.xml';
 
 function component() {
 	var element = document.createElement('div');
@@ -8,6 +10,14 @@ function component() {
 	element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 	// 给元素添加 hello 样式类
 	element.classList.add('hello');
+
+	//将图像添加到我们现有的 div
+	var myIcon = new Image();
+	myIcon.src = Icon;
+
+	element.appendChild(myIcon);
+
+	console.log(Data);
 
 	return element;
 }
