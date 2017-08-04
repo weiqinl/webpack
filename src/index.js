25e2127e7a944362e1323402f052c687f3872956
刘伟勤 <wbliuwq@yonyou.com>
2017-08-04 17:13:28 +0800

    管理输出

--- src/index.js
+++ src/index.js
@@ -1,24 +1,18 @@
 import _ from 'lodash';
-import './style.css';
-import Icon from './logo.png';
-import Data from './data.xml';
+import printMe from './print.js';
 
 function component() {
 	var element = document.createElement('div');
+	var btn = document.createElement('button');
 
 	// Lodash 是由当前 script 脚本 import 导入进来的
 	element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-	// 给元素添加 hello 样式类
-	element.classList.add('hello');
+	
+	btn.innerHTML = 'Click me and check the console!';
+	btn.onclick = printMe;
 
-	//将图像添加到我们现有的 div
-	var myIcon = new Image();
-	myIcon.src = Icon;
-
-	element.appendChild(myIcon);
-
-	console.log(Data);
-
+	element.appendChild(btn);
+	 
 	return element;
 }
 
