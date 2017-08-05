2207608f40e83e475f7eddc62c4d86fe37f0b73a
weiqinl <772863869@qq.com>
2017-08-05 09:36:59 +0800

    update: webpack-dev-server  为你提供了一个简单的 web 服务器，并且能够实时重新加载(live reloading)

--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,9 @@ module.exports = {
 		print: './src/print.js',
 	},
 	devtool: 'inline-source-map',
+	devServer: {
+		contentBase: './dist' //此配置告知 webpack-dev-server，在 localhost:8080 下建立服务，将 dist 目录下的文件，作为可访问文件。
+	},
 	plugins: [
 		new CleanWebpackPlugin(['dist']),//清理dist文件夹
 		new HtmlWebpackPlugin({
