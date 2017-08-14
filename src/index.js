7c65ac526615b2aec2b62f5bbcd5eee3d4dee439
刘伟勤 <wbliuwq@yonyou.com>
2017-08-14 13:30:14 +0800

    起步的示例，管理输出的plugins 来测试 缓存

--- src/index.js
+++ src/index.js
@@ -3,18 +3,7 @@ import _ from 'lodash';
 function component() {
 
     var element = document.createElement('div');
-    var button = document.createElement('button');
-    var br = document.createElement('br');
-
-    button.innerHTML = 'Click me and look at the console!';
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.appendChild(br);
-    element.appendChild(button);
-
-    button.onclick = e => import(/* webpackChunkName: 'print' */ './print.js').then(module => {
-    	var print = module.default;
-    	print();
-    })
 
     return element;
 
--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
 	entry: {
 		index: './src/index.js'
 	},
-	devtool: 'inline-source-map',
+	devtool: 'inline-source-map',//此选项控制是否生成，以及如何生成 source map。
 	devServer: {
 		contentBase: path.resolve(__dirname, 'dist'), //此配置告知 webpack-dev-server，在 localhost:8080 下建立服务，将 dist 目录下的文件，作为可访问文件。
 		hot: true, //告诉dev-server 我们在使用 HMR
@@ -16,12 +16,12 @@ module.exports = {
 	plugins: [
 		new CleanWebpackPlugin(['dist']),//清理dist文件夹
 		new HtmlWebpackPlugin({
-			title: 'Code Splitting-weiqinl'
+			title: 'Caching-weiqinl'
 		}),
 		new webpack.HotModuleReplacementPlugin(), //启用HMR
 	],
 	output: {
-		filename: '[name].bundle.js',
+		filename: '[name].[hash].js',
 		chunkFilename: '[name].bundle.js',
 		path: path.resolve(__dirname, 'dist')
 	},
