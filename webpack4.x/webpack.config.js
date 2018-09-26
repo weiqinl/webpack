d86a7bccca93b331f31a93d43e7db74fe4c40f6c
weiqinl <772863869@qq.com>
2018-09-26 22:39:26 +0800

    A: webpack构建-起步阶段

---
+++ webpack4.x/webpack.config.js
@@ -0,0 +1,9 @@
+const path = require('path') // nodejs模块，处理文件路径
+
+module.exports = { 
+  entry: './src/index.js', // 入口起点
+  output: { //  出口/输出
+    filename: 'main.js', // 输出文件名称
+    path: path.resolve(__dirname, 'dist') // 输出的目录
+  }
+}
