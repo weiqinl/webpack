0b9ee6f2603b89b685e5d3cb3be2ce02c593b807
weiqinl <772863869@qq.com>
2018-09-26 22:51:43 +0800

    A: 加载样式到head

--- webpack4.x/src/index.js
+++ webpack4.x/src/index.js
@@ -1,6 +1,9 @@
+import './style.css'
+
 function component() {
   let element = document.createElement('div')
   element.innerHTML = 'Hello webpack' + new Date()
+  element.classList.add('hello')
   return element
 }
 document.body.appendChild(component())
