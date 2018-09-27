b026caba67cab1e713d77b525ee59bdde0001f1e
weiqinl <772863869@qq.com>
2018-09-27 17:39:25 +0800

    A: 环境配置分离 开发环境和生产环境使用两个配置文件，共同的配置提取到一个公共配置文件中 不同环境和公共配置文件使用webpack-merge插件合并

--- webpack4.x/webpack.common.js
+++ webpack4.x/webpack.common.js
@@ -1,14 +1,11 @@
 const path = require('path') // nodejs模块，处理文件路径
 const HtmlWebpackPlugin = require('html-webpack-plugin') // 生成html
 const CleanWebpackPlugin = require('clean-webpack-plugin') // 清理文件夹
-const webpack = require('webpack')
 
 module.exports = {
-  mode: 'production',
   entry: { // 入口起点
     app: './src/index.js'
   },
-  devtool: 'inline-source-map', // source-map 选项
   devServer: {
     contentBase: './dist',
     hot: true
@@ -24,15 +21,12 @@ module.exports = {
   output: { //  出口/输出
     filename: '[name].bundle.js', // 输出文件名称
     path: path.resolve(__dirname, 'dist'), // 相对publicPath的输出的目录
-    publicPath: '/' // 输出的目录
   },
   plugins: [
     new CleanWebpackPlugin(['dist']),
     new HtmlWebpackPlugin({
-      title: 'webpack-Output-Management-weiqinl',
+      title: 'webpack--production-weiqinl',
       filename: 'index.html' // 相对于输出目录
-    }),
-    new webpack.NamedModulesPlugin(),
-    new webpack.HotModuleReplacementPlugin()
+    })
   ]
 }
