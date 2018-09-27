998d60c4077e4fdd1b7d279d8d83f3878827b338
weiqinl <772863869@qq.com>
2018-09-27 08:39:40 +0800

    A: 增加clean-webpack-plugin插件 清理文件夹

--- webpack4.x/webpack.config.js
+++ webpack4.x/webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path') // nodejs模块，处理文件路径
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin') // 生成html
+const CleanWebpackPlugin = require('clean-webpack-plugin') // 清理文件夹
 
 module.exports = {
   entry: { // 入口起点
@@ -11,6 +12,7 @@ module.exports = {
     path: path.resolve(__dirname, 'dist') // 输出的目录
   },
   plugins: [
+    new CleanWebpackPlugin(['dist']),
     new HtmlWebpackPlugin({
       title: 'webpack-Output-Management-weiqinl',
       filename: 'index.html' // 相对于输出目录
