a0ea7040e2f20b55233223a35baf07baf928bd47
weiqinl <772863869@qq.com>
2018-09-27 09:46:20 +0800

    A: webpack --watch 可以监控文件的变化

--- webpack4.x/src/print.js
+++ webpack4.x/src/print.js
@@ -1,4 +1,4 @@
 export default function printMe() {
   console.log('I get called from print.js!-logInfo');
-  console1.error('I get called from print.js!-errorInfo');
+  console.error('I get called from print.js!-errorInfo');
 }
