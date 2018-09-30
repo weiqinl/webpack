4193b57ddff452cf1da6abd2f47cfff06e8fcf2f
weiqinl <772863869@qq.com>
2018-09-30 09:03:59 +0800

    Add: 缓存 提取模板(Extracting Boilerplate) 将optimization.runtimeChunk 设置为single 即可创建一个运行时包。

--- webpack4.x/webpack.common.js
+++ webpack4.x/webpack.common.js
@@ -1,6 +1,7 @@
 const path = require('path') // nodejs模块，处理文件路径
 const CleanWebpackPlugin = require('clean-webpack-plugin') // 清理文件夹
 const HtmlWebpackPlugin = require('html-webpack-plugin') // 生成html
+const webpack = require('webpack')
 
 module.exports = {
   entry: { // 入口起点
@@ -9,14 +10,17 @@ module.exports = {
   plugins: [
     new CleanWebpackPlugin(['dist']),
     new HtmlWebpackPlugin({
-      title: 'webpack--Code-Splitting-weiqinl',
+      title: 'webpack--Caching-weiqinl',
       filename: 'index.html' // 相对于输出目录
     })
   ],
   output: { //  出口/输出
-    filename: '[name].bundle.js', // 输出文件名称
-    chunkFilename: '[name].bundle.js',
+    filename: '[name].[chunkhash].js', // 每个输出bundle文件名称(包含入口起点、代码拆分或各种插件创建的多个bundle)
+    chunkFilename: '[name].bundle.js', // 按需加载的chunk的输出文件名称
     path: path.resolve(__dirname, 'dist'), // 相对publicPath的输出的目录
     publicPath: '/'
+  },
+  optimization: {
+    runtimeChunk: 'single'
   }
 }
